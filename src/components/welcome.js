051474b829add390edfba847ec8ea81a3085cdc4
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-10-23 18:37:27 +0200

    Specified font styles, colors and added google fonts to project

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -2,5 +2,5 @@ import React, { Component } from 'react';
 import Typekit from 'react-typekit';
 
 export default () => <div className="">
-  <h1>Welcome to this page!</h1>
+  <h2>Welcome to this page!</h2>
 </div>
