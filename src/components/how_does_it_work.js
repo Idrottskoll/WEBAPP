e739a2305935c78fcc389f0973063a5e755109cf
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-10-27 13:24:28 +0200

    Added icons to project and included it in markup

--- src/components/how_does_it_work.js
+++ src/components/how_does_it_work.js
@@ -4,7 +4,7 @@ class HowDoesItWork extends Component {
   render () {
     return (
       <div className="container">
-        <div className="row">
+        <div className="row module">
           <div className="col-md-12 title">
             <h2>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt .
@@ -12,29 +12,38 @@ class HowDoesItWork extends Component {
           </div>
         </div>
         <div className="row steps">
-          <div className="col-md-4 step">
-            <h3>
-              1. Lägg order
-            </h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-            </p>
+          <div className="col-md-4">
+            <div className="step">
+              <img src={'src/assets/img/stepOne.png'} className="stepIcons"/>
+              <h3>
+                1. Lägg order
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </p>
+            </div>
           </div>
-          <div className="col-md-4 step">
-            <h3>
-              2. Videon filmas
-            </h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-            </p>
+          <div className="col-md-4">
+            <div className="step">
+              <img src={'src/assets/img/stepTwo.png'} className="stepIcons"/>
+              <h3>
+                2. Videon filmas
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </p>
+            </div>
           </div>
-          <div className="col-md-4 step">
-            <h3>
-              3. Titta på videon
-            </h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-            </p>
+          <div className="col-md-4">
+            <div className="step">
+              <img src={'src/assets/img/stepThree.png'} className="stepIcons"/>
+              <h3>
+                3. Titta på videon
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </p>
+            </div>
           </div>
         </div>
       </div>
