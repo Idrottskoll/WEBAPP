ba3fc61c12ba2d2bdc805d124b82101018bc8f05
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-10-27 14:45:08 +0200

    Fixed style for case slider

--- src/components/caseSlider.js
+++ src/components/caseSlider.js
@@ -13,6 +13,9 @@ export default class CaseSlider extends Component {
     };
     return (
       <div>
+        <div className="container caseSlider">
+          <h2>Några turneringar som idrottskoll har filmat</h2>
+        </div>
         <Slider {...settings}>
           <div className="col-md-2"><div className="case"><h4>Junior sm 2016 i squash</h4></div></div>
           <div className="col-md-2"><div className="case"><h4>Junior sm 2016 i squash</h4></div></div>
