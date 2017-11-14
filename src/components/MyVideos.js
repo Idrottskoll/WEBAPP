c0e914243f1d6cb534b1b22ae7c23f61668b91ce
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-11-14 20:09:17 +0100

    Added demo videos with default pictures

--- src/components/MyVideos.js
+++ src/components/MyVideos.js
@@ -66,37 +66,38 @@ class MyVideos extends Component {
           <h2>Mina videos</h2>
         </div>
         <div className="col-md-4">
-          <div className="video-block"></div>
+          <div className="video-block squash-default">
+            <img src={'src/assets/img/play-button-transparent-2x.png'} className="play"/>
+          </div>
           {/* {this.renderInformation()} */}
-
           {/* {renderInformation(props.data)} */}
-        </div>
-        <div className="col-md-4">
-          <div className="video-block"></div>
           <div className="video-description">
-            <h4>SM i Rugby</h4>
+            <h4>Squash, Lerumstk Bana 4</h4>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.<br /><br />
-              Datum: 20170929<br />
-              Plats: LandalaSquash<br />
-              Speltid:1610-1615<br />
-              Bana: 01<br />
-              Dagar kvar på hemsidan: +31 dagar
+              Sport: Squash<br />
+              Klubb: Lerumstk<br />
+              Inspelat: 2017-09-01 13:16<br />
+              Bana: 3
             </p>
           </div>
         </div>
         <div className="col-md-4">
-          <div className="video-block"></div>
+          <div className="video-block tennis-default">
+            <img src={'src/assets/img/play-button-transparent-2x.png'} className="play"/>
+          </div>
           <div className="video-description">
-            <h4>SM i Rugby</h4>
+            <h4>Tennis, Lerumstk Bana 3</h4>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.<br /><br />
-              Datum: 20170929<br />
-              Plats: LandalaSquash<br />
-              Speltid:1610-1615<br />
-              Bana: 01<br />
-              Dagar kvar på hemsidan: +31 dagar
+              Sport: Tennis<br />
+              Klubb: Lerumstk<br />
+              Inspelat: 2017-11-01 13:16<br />
+              Bana: 3
             </p>
+          </div>
+        </div>
+        <div className="col-md-4">
+          {/* <div className="video-block"></div> */}
+          <div className="video-description">
           </div>
         </div>
       </div>
